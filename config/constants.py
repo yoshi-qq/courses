9fe55ccccbaf7669556ede37eba27b178295e8ba
Christian <abel.christian.acc@gmail.com>
2025-04-14 19:49:40 +0200

    OverviewSheet and Course class added

--- config/constants.py
+++ config/constants.py
@@ -1,41 +1,83 @@
+from enum import Enum
 from types import UnionType
-from typing import Union, Literal
+from typing import Literal
 
-lettersToA = Literal['a']
-lettersToB = Literal['a', 'b']
-lettersToC = Literal['a', 'b', 'c']
-lettersToD = Literal['a', 'b', 'c', 'd']
-lettersToE = Literal['a', 'b', 'c', 'd', 'e']
-lettersToF = Literal['a', 'b', 'c', 'd', 'e', 'f']
+LettersToA = Literal['a']
+LettersToB = Literal['a', 'b']
+LettersToC = Literal['a', 'b', 'c']
+LettersToD = Literal['a', 'b', 'c', 'd']
+LettersToE = Literal['a', 'b', 'c', 'd', 'e']
+LettersToF = Literal['a', 'b', 'c', 'd', 'e', 'f']
 
-
-
-RowNumber = Union[]
-viableRowNumbers: list[int] = [1, 2, 3, 10, 11, 12, 13, 14, 15, 21, 24, 25, 26, 27, 32, 33, 34, 37, 38, 39, 40, 41, 42, 52, 53, 54]
-viableRowLetters = {
-    1: lettersToF,
-    2: lettersToF,
-    3: lettersToF,
-    10: lettersToF,
-    11: lettersToF,
-    12: lettersToD,
-    13: lettersToC,
-    14: lettersToF,
-    15: lettersToF,
-    21: lettersToC,
-    24: lettersToD,
-    25: lettersToD,
-    26: lettersToF,
-    27: lettersToF,
-    32: lettersToF,
-    33: lettersToF,
-    37: lettersToB,
-    38: lettersToB,
-    39: lettersToB,
-    40: lettersToB,
-    41: lettersToB,
-    42: lettersToB,
-    52: lettersToF,
-    53: lettersToF,
-    54: lettersToD
+RowNumber = Literal[1, 2, 3, 10, 11, 12, 13, 14, 15, 21, 24, 25, 26, 27, 32, 33, 34, 37, 38, 39, 40, 41, 42, 52, 53, 54]
+ViableRowLetters: dict[RowNumber, UnionType] = {
+    1: LettersToF,
+    2: LettersToF,
+    3: LettersToF,
+    10: LettersToF,
+    11: LettersToF,
+    12: LettersToD,
+    13: LettersToC,
+    14: LettersToF,
+    15: LettersToF,
+    21: LettersToC,
+    24: LettersToD,
+    25: LettersToD,
+    26: LettersToF,
+    27: LettersToF,
+    32: LettersToF,
+    33: LettersToF,
+    37: LettersToB,
+    38: LettersToB,
+    39: LettersToB,
+    40: LettersToB,
+    41: LettersToB,
+    42: LettersToB,
+    52: LettersToF,
+    53: LettersToF,
+    54: LettersToD
 }
+
+StartingGrade = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+class ForeignLanguage(Enum):
+    ENGLISH = 0
+    FRENCH = 1
+    SPANISH = 2
+
+ClassLetter = Literal['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+
+class Subject(Enum):
+    GERMAN = 0
+    ENGLISH = 1
+    FRENCH = 2
+    LATIN = 3
+    SPANISH = 4
+    MUSIC = 5
+    ART = 6
+    THEATER = 7
+    ENGLISH_Z = 8
+    MUSIC_Z = 9
+    
+    POLITICS = 10
+    HISTORY = 11
+    GEOGRAPHY = 12
+    PHILOSOPHY = 13
+    
+    MATHEMATICS = 14
+    PHYSICS = 15
+    CHEMISTRY = 16
+    BIOLOGY = 17
+    INFORMATICS = 18
+    BIOLOGY_Z = 19
+    
+    PHYSICAL_EDUCATION = 20
+    SKIING = 21
+    SURFING = 22
+    SUB = 23
+
+class PPType(Enum):
+    PP = 0
+    BLL = 1
+    bf = 2
+    
--- types/types.py
+++ types/types.py
@@ -1,3 +1,22 @@
-from config.constants import viableRowNumbers, viableRowLetters
+from typing import Literal, Optional
+from config.constants import RowNumber, LettersToF, ForeignLanguage, StartingGrade, ClassLetter, Subject, PPType
+
+class Course:
+    def __init__(self, subject: Subject, PP: Optional[PPType], examSubject: Literal[1, 2, 3, 4], mandatoryCourses: Optional[Literal[1, 2, 3, 4]], semesters: tuple[bool, bool, bool, bool], weekPeriods: int, creditAmount: int) -> None:
+        self.subject = subject
+        self.PP = PP
+        self.examSubject = examSubject
+        self.mandatoryCourses = mandatoryCourses
+        self.semesters = semesters
+        self.weekPeriods = weekPeriods
+        self.creditAmount = creditAmount
+
 class OverviewSheet:
-    def __init__(self, name: str, surname: str, row: tuple[int, str])
+    def __init__(self, name: str, surname: str, row: tuple[RowNumber, LettersToF], foreignLanguages: list[tuple[ForeignLanguage, StartingGrade]], entryyear: int, classLetter: ClassLetter, courses: list[Course]) -> None:
+        self.name = name
+        self.surname = surname
+        self.row = row
+        self.foreignLanguages = foreignLanguages
+        self.entryyear = entryyear
+        self.classLetter = classLetter
+        self.courses = courses
